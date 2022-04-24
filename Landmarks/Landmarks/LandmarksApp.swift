f8fb1e7e485942b2f6a5fe0b50363f6a2f78cc24
Jeong Jaeyoung <shaula@mensakorea.org>
2022-04-25 00:16:14 +0900

    220425_Section4

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jeong on 2022/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
